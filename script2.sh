e2856eee028583141bc1193dcb494ebad16af94e
Josealqd <joalmeidaan@uide.edu.ec>
2025-12-04 13:17:01 +0000

    segundo

---
+++ script2.sh
@@ -0,0 +1,8 @@
+uname -a	
+#Identificación del Kernel	Versión del Sistema Operativo y la arquitectura de la máquina.
+lscpu	
+#Información de la CPU	Detalles sobre el procesador: modelo, número de núcleos, velocidad, etc.
+free -h	
+#Uso de la RAM	Estado de la memoria física y swap, en un formato legible para humanos (-h).
+df -h	
+#Uso del Disco	Espacio utilizado y disponible en los sistemas de archivos montados, en formato legible.
